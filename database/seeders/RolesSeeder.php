753730538ea9dc97ed653c4c15d82a991b447dd5
Curtis Delicata <curtis.delicat@outlook.com>
2026-03-20 15:01:45 +0000

    Update UserSeeder and RoleSeeder

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Team;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
+use BezhanSalleh\FilamentShield\Support\Utils;
 
 class RolesSeeder extends Seeder
 {
@@ -13,47 +15,19 @@ class RolesSeeder extends Seeder
      */
     public function run(): void
     {
-        // Get all available permissions
-        $allPermissions = Permission::where('guard_name', 'web')->pluck('id')->toArray();
+        $roleData = [
+            'name' => 'super_admin',
+            'guard_name' => 'web',
+        ];
 
-        // Create super_admin role (as defined in Shield config) with all permissions
-        $superAdminRole = Role::firstOrCreate(['name' => 'super_admin']);
-        $superAdminRole->syncPermissions($allPermissions);
-
-        // Create admin role with all permissions (can be customized later)
-        $adminRole = Role::firstOrCreate(['name' => 'admin']);
-        $adminRole->syncPermissions($allPermissions);
-
-        // Create panel_user role (as defined in Shield config) - basic panel access
-        $panelUserRole = Role::firstOrCreate(['name' => 'panel_user']);
-        $viewPermissions = $this->getViewOnlyPermissions();
-        $panelUserRole->syncPermissions($viewPermissions);
-        
-        // Create free role - limited permissions for free tier users
-        $freeRole = Role::firstOrCreate(['name' => 'free']);
-        $freePermissions = $this->getViewOnlyPermissions(['%User%']);
-        $freeRole->syncPermissions($freePermissions);
-    }
-
-    /**
-     * Get view-only permissions, optionally excluding certain patterns.
-     *
-     * @param array $excludePatterns Patterns to exclude from the result
-     * @return array
-     */
-    protected function getViewOnlyPermissions(array $excludePatterns = []): array
-    {
-        $query = Permission::where('guard_name', 'web')
-            ->where(function ($q) {
-                $q->where('name', 'like', 'ViewAny::%')
-                  ->orWhere('name', 'like', 'View::%');
-            });
-
-        // Apply exclusion patterns if provided
-        foreach ($excludePatterns as $pattern) {
-            $query->where('name', 'not like', $pattern);
+        if (Utils::isTenancyEnabled()) {
+            $team = Team::firstOrFail();
+            $roleData["team_id"] = $team->id;
         }
 
-        return $query->pluck('id')->toArray();
+        $adminRole = Role::firstOrCreate($roleData);
+
+        $permissions = Permission::where('guard_name', 'web')->pluck('id')->toArray();
+        $adminRole->syncPermissions($permissions);
     }
 }
